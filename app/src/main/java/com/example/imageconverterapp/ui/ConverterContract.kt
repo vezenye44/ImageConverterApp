8343407855b9fcf221916fc64c2c732f0d7fe2a7
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-17 14:01:53 +0300

    Создание контракта c интерфейсами для Presenter и View.

---
+++ app/src/main/java/com/example/imageconverterapp/ui/ConverterContract.kt
@@ -0,0 +1,39 @@
+package com.example.imageconverterapp.ui
+
+import android.graphics.Bitmap
+import android.net.Uri
+
+interface ConverterContract {
+    interface View {
+        // Запуск ActivityResultLauncher.launch()
+        fun launchOpenFile()
+        fun launchSaveFile()
+
+        fun showOriginalImage(bitmap: Bitmap)
+        fun showConvertedImage(bitmap: Bitmap)
+        fun showError(throwable: Throwable)
+
+        fun openFile(uri: Uri)
+        fun saveFile(uri: Uri, bitmapXByteArray: ByteArray)
+
+        fun convertBtnEnable(isEnable: Boolean)
+        fun saveBtnEnable(isEnable: Boolean)
+
+    }
+
+    interface Presenter {
+        fun attach(viewState: View)
+        fun detach()
+
+        // Коллбеки для registerForActivityResult()
+        val openLauncherCallback: (Uri?) -> Unit
+        val saveLauncherCallback: (Uri?) -> Unit
+
+        fun onOpenImageClick()
+        fun onSaveImageClick()
+        fun onConvertImageClick()
+
+        fun openFileSuccess(bitmap: Bitmap)
+        fun saveFileSuccess()
+    }
+}
